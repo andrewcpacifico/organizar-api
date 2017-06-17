671ada6906082f8303ebee9daec2842f0919b0fe
Andrew Pacifico <andrewcpacifico@gmail.com>
2017-06-17 14:16:58 -0400

    style(TaskController): change code style

--- src/controllers/task.js
+++ src/controllers/task.js
@@ -1,11 +1,11 @@
 const Task = require('../models/task');
 
-const TaskController = {};
-
-TaskController.getAll = function getAll(req, res) {
-  Task.getAll().then((tasks) => {
-    res.json(tasks);
-  });
+const TaskController = {
+  getAll(req, res) {
+    Task.getAll().then((tasks) => {
+      res.json(tasks);
+    });
+  },
 };
 
 module.exports = TaskController;
